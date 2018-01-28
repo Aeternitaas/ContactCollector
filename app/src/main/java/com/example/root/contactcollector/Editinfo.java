865eabb4c8026a057b43daba1ae8a3bceae49b9a
Robin Avenoso <rxa8448@cs.rit.edu>
2018-01-27 22:10:45 -0500

    Added funconality to Editinfo

--- app/src/main/java/com/example/root/contactcollector/Editinfo.java
+++ app/src/main/java/com/example/root/contactcollector/Editinfo.java
@@ -6,22 +6,27 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Editinfo extends AppCompatActivity {
+
+    Info temp = new Info();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editinfo);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final EditText nameedit = (EditText) findViewById(R.id.name);
+        nameedit.setText(temp.getName());
+
+        Button saveButton = (Button) findViewById(R.id.save);
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                temp.setName(nameedit.getText().toString());
+
             }
         });
     }
